756ca7e707da562d06fa097a49c3a96e073e3023
moling <365024424@qq.com>
2017-09-30 13:45:21 +0800

    实现爬取一个职位的数据

--- spider.py
+++ spider.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 # Date    : 2017-09-29 10:33:20
 # Author  : moling3650
+import re
 import time
 import random
 import requests
@@ -23,16 +24,43 @@ class ZhiLianSpider(object):
         self.search_URL = next_page.attrs['href'] if (next_page and next_page.has_attr('href')) else ''
 
 
-    def getJobURLs(self):
+    def _getJobURLs(self):
         r = self.session.get(self.search_URL, timeout=10)
         soup = BeautifulSoup(r.text, 'html.parser')
         self._setNextSearchURL(soup)
         return (el.attrs['href'] for el in soup.select('.zwmc a[href^="http://jobs.zhaopin.com/"]'))
 
+    def _getDataByURL(self, url):
+        data = {}
+        try: 
+            r = self.session.get(url, timeout=10)
+            soup = BeautifulSoup(r.text, 'html.parser')
+            data['id'] = re.compile("http.+/|\.htm.*").sub('', url)
+            data['职位链接'] = url
+            data['职位名称'] = soup.select_one('.fl > h1').text.strip()
+            data['公司名称'] = soup.select_one('.fl > h2 > a').text.strip()
+            data['公司福利'] = '/'.join(el.text.strip() for el in soup.select('.welfare-tab-box > span'))
+            tab = soup.select_one('.tab-inner-cont')
+            data['职位描述'] =  '\n'.join(p.text.strip() for p in tab.find_all('p'))
+            data['详细工作地点'] = tab.find('h2').contents[0].strip()
+            fields = (el.text.strip().replace('：', '') for el in soup.select('.terminal-ul li > span'))
+            values = (el.text.strip() for el in soup.select('.terminal-ul li > strong'))
+            data.update(dict(zip(fields, values)))
+        except e:
+            print('timeout: %s' % url)
+            
+        return data
+
+
+    def main(self):
+        urls = self._getJobURLs()
+        for url in urls: 
+            time.sleep(random.randint(3, 5))
+            data = self._getDataByURL(url)
+            if data:
+                print(data['职位描述'])
+
 
 if __name__ == '__main__':
     spider = ZhiLianSpider('前端', job_location='深圳')
-    while spider.search_URL:
-        time.sleep(random.randint(1, 3))
-        print(spider.search_URL)
-        spider.getJobURLs()
+    spider.main()
